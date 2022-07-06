973b13c2938052622a789fa8586c4dcf6eee2b46
Q1H1 <95531058+Q1H1@users.noreply.github.com>
2022-07-06 22:19:22 +0100

    Add files via upload

---
+++ MC.py
@@ -0,0 +1,155 @@
+from re import A
+
+def run():
+    character = input("Enter a single character \n")
+    character = character.lower()
+
+    c0 = '-----'
+    c1 = '.----'
+    c2 = '..---'
+    c3 = '...--'
+    c4 = '....-'
+    c5 = '.....'
+    c6 = '-....'
+    c7 = '--...'
+    c8 = '---..'
+    c9 = '----.'
+    ca = '.-'
+    cb = '-...'
+    cc = '-.-.'
+    cd = '-..'
+    ce = '.'
+    cf = '..-.'
+    cg = '--.'
+    ch = '....'
+    ci = '..'
+    cj = '.---'
+    ck = '-.-'
+    cl = '.-..'
+    cm = '--'
+    cn = '-.'
+    co = '---'
+    cp = '.--.'
+    cq = '--.-'
+    cr = '.-.'
+    cs = '...'
+    ct = '-'
+    cu = '..-'
+    cv = '...-'
+    cw = '.--'
+    cx = '-..-'
+    cy = '-.--'
+    cz = '--..'
+
+    if character == "0":
+        print(c0)
+        run()
+    elif character == "1":
+        print(c1)
+        run()
+    elif character == "2":
+        print(c2)
+        run()
+    elif character == "3":
+        print(c3)
+        run()
+    elif character == "4":
+        print(c4)
+        run()
+    elif character == "5":
+        print(c5)
+        run()
+    elif character == "6":
+        print(c6)
+        run()
+    elif character == "7":
+        print(c7)
+        run()
+    elif character == "8":
+        print(c8)
+        run()
+    elif character == "9":
+        print(c9)
+        run()
+    elif character == "a":
+        print(ca)
+        run()
+    elif character == "b":
+        print(cb)
+        run()
+    elif character == "c":
+        print(cc)
+        run()
+    elif character == "d":
+        print(cd)
+        run()
+    elif character == "e":
+        print(ce)
+        run()
+    elif character == "f":
+        print(cf)
+        run()
+    elif character == "g":
+        print(cg)
+        run()
+    elif character == "h":
+        print(ch)
+        run()
+    elif character == "i":
+        print(ci)
+        run()
+    elif character == "j":
+        print(cj)
+        run()
+    elif character == "k":
+        print(ck)
+        run()
+    elif character == "l":
+        print(cl)
+        run()
+    elif character == "m":
+        print(cm)
+        run()
+    elif character == "n":
+        print(cn)
+        run()
+    elif character == "o":
+        print(co)
+        run()
+    elif character == "p":
+        print(cp)
+        run()
+    elif character == "q":
+        print(cq)
+        run()
+    elif character == "r":
+        print(cr)
+        run()
+    elif character == "s":
+        print(cs)
+        run()
+    elif character == "t":
+        print(ct)
+        run()
+    elif character == "u":
+        print(cu)
+        run()
+    elif character == "v":
+        print(cv)
+        run()
+    elif character == "w":
+        print(cw)
+        run()
+    elif character == "x":
+        print(cx)
+        run()
+    elif character == "y":
+        print(cy)
+        run()
+    elif character == "z":
+        print(cz)
+        run()
+    else:
+        run()
+
+run()
